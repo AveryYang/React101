611e599d09c2ac19703f49c7961ca662a57c62d0
avery_yang <avery_yang@asus.com>
2018-01-18 16:40:43 +0800

    [Moko][React101][4] The First React Component

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,14 @@
 "use strict";
 
-ReactDOM.render(React.createElement("h1", null, "Hello!"), document.getElementById('example'));
+var Title = React.createClass({ displayName: "Title",
+    render: function render() {
+        return (
+            // 元件的render函數 所return的參數只是能一個element，但h1&h2都各為一個element，所以需包進div避免造成錯誤
+            React.createElement("div", null, React.createElement("h1", null, "Title"), React.createElement("h2", null, "Author"))
+        );
+    }
+});
+ReactDOM.render(
+// 物件名稱必需是大寫
+React.createElement(Title, null), document.getElementById('example'));
 //# sourceMappingURL=app.js.map
--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,16 @@
+var Title = React.createClass({
+    render: function () {
+        return (
+            // 元件的render函數 所return的參數只是能一個element，但h1&h2都各為一個element，所以需包進div避免造成錯誤
+            <div>
+                <h1>Title</h1>
+                <h2>Author</h2>
+            </div>
+        )
+    }
+});
 ReactDOM.render(
-<h1>Hello!</h1>,
-document.getElementById('example')
+    // 物件名稱必需是大寫
+    <Title />,
+    document.getElementById('example')
 );
