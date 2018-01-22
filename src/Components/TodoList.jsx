73bf5d2166de8fc5b556c576b80f5b998b5dc20a
avery_yang <avery_yang@asus.com>
2018-01-22 16:42:47 +0800

    [Moko][React101][17-19] Todo Application

---
+++ src/Components/TodoList.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+export class TodoList extends Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    componentDidMount() {
+
+    }
+    render() {
+        return(
+            <div>
+                <p>{this.props.items}</p>
+            </div>
+
+        );
+    }
+}
+
+TodoList.defaultProps = {
+
+}
